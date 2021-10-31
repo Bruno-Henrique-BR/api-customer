1a96977f974405256f11960e60dbe8e109c08232
Bruno-Henrique-BR <Brunohenrique2208sb@gmail.com>
2021-10-31 16:47:34 -0300

    Criando os services e os teste

--- src/main/java/com/apicustomer/tgroup/controller/AddressController.java
+++ src/main/java/com/apicustomer/tgroup/controller/AddressController.java
@@ -23,44 +23,7 @@ import java.util.List;
 public class AddressController {
 
     @Autowired
-    AddressRepository addressRepository;
-    @Autowired
-    CustomerRepository customerRepository;
-    @Autowired
     AddressService addressService;
-
-    /*@ApiOperation(value = "Retorna uma lista de Address")
-    @GetMapping("/{id}")
-    public List<Customer> listaAddress(@PathVariable(value = "id") long id) {
-        return customerRepository.findAllById(Collections.singleton(id));
-    }
-
-    @ApiOperation(value = "Retorna a lista de Address do Customer ")
-    @GetMapping("/addressByCustomer/{id}")
-    public Collection<Address> GetAddressByCustomer(@PathVariable(value = "id") long id) {
-        return addressRepository.getByAddressesCustomer(id);
-    }
-
-
-
-    @ApiOperation(value = "Consulta um adress")
-    @GetMapping("/address/{id}")
-    public Address GetAddressId(@PathVariable(value = "id") long id) {
-        return addressRepository.findById(id);
-    }
-
-    @ApiOperation(value = "Cria um adress")
-    @PostMapping("/address")
-    public Address createAddress(@RequestBody @Valid Address address) {
-        return addressRepository.save(address);
-    }
-
-    @ApiOperation(value = "Deleta um adress")
-    @DeleteMapping("/address/{id}")
-    public Address deleteAddress(@PathVariable(value = "id") long id) {
-        return addressRepository.deleteById(id);
-    }
-    */
 
     @ApiOperation(value = "Retorna uma lista de addresses")
     @GetMapping("/{id}")
--- src/main/java/com/apicustomer/tgroup/controller/CustomerController.java
+++ src/main/java/com/apicustomer/tgroup/controller/CustomerController.java
@@ -19,36 +19,8 @@ import java.util.List;
 @Api(value = "API REST Customers")
 public class CustomerController {
     @Autowired
-    CustomerRepository customerRepository;
-
-    @Autowired
     private CustomerService customerService;
 
-
-
-    /*@ApiOperation(value = "Consulta um customer")
-    @GetMapping("/customer/{id}")
-    public Customer GetPessoaById(@PathVariable(value = "id") long id) {
-        return customerRepository.findById(id);
-    }
-
-    @ApiOperation(value = "Cria um customer")
-    @PostMapping("/customer")
-    public Customer createCustomer(@RequestBody @Valid Customer customer) {
-        return customerRepository.save(customer);
-    }
-
-    @ApiOperation(value = "Deleta um customer")
-    @DeleteMapping("/customer/{id}")
-    public Customer deleteCustomer(@PathVariable(value = "id") long id) {
-        return customerRepository.deleteById(id);
-    }
-
-    @ApiOperation(value = "Atualiza um customer")
-    @PutMapping("/customer")
-    public Customer updateCustomer(@RequestBody @Valid Customer customer) {
-        return customerRepository.save(customer);
-    }*/
 
     @ApiOperation(value = "Retorna uma lista de Customers")
     @GetMapping("/customers")
